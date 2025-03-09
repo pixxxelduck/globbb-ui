82430fd6ebc4ec0fdb7605d3d23bd4e9ac5b42b7
Waldemart 🤘 <catindev@gmail.com>
2025-03-09 20:56:31 +0200

    Done: лэндос, страница регистрации. Блок с формой

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Landing from "./Pages/Landing/Landing";
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
     <>
-      <Landing />
+      {/* <Landing /> */}
+      <SignUp />
     </>
   );
 }
---
+++ src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,36 @@
+import "../../Blocks/Logotype/Logotype.css";
+import FormStyles from "../../Blocks/Form/Form.module.css";
+import Buttons from "../../Blocks/Buttons/Buttons.module.css";
+import styles from "./SignUp.module.css";
+
+function SignUp() {
+  return (
+    <>
+      <div className={styles.SignUp}>
+        <div className={styles.Picture}></div>
+        <div className={styles.Content}>
+          <div className="Logotype"></div>
+          <form className={[FormStyles.Form, styles.Form].join(" ")}>
+            <h1>How can we introduce you?</h1>
+            <p>
+              Enter your username and email address and we'll send you a
+              one-time magic link. By clicking the link below, you will
+              automatically link your data to your email.
+            </p>
+            <div className={FormStyles.Field}>
+              <input type="text" placeholder="username" />
+            </div>
+            <div className={FormStyles.Field}>
+              <input type="text" placeholder="your@mail.com" disabled={true} />
+            </div>
+            <div className={Buttons.Buttons}>
+              <button className={Buttons.Big}>Link email with Globbb</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
